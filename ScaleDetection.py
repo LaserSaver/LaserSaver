72ec0677d7e2a89f8c2f2efcb92993e3ce392fe5
luke <lwvyruz@gmail.com>
2015-11-02 12:21:50 -0700

    skeleton

---
+++ ScaleDetection.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+import sys
+
+
+class ScaleDetection:
+    def __init__(self):
+        x_scale = None
+        y_scale = None
+
+    def calibrate(self, image, known_x, known_y):
+        pass
+
+    def loadConfigFile(self, config="scale.config"):
+        pass
+
+    def detectSize(self, image):
+        pass
