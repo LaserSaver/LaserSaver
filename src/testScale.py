1ddb50a7726d57808104b774eae43242edb2d212
luke <lwvyruz@gmail.com>
2015-11-02 17:48:17 -0700

    more tests

--- src/testScale.py
+++ src/testScale.py
@@ -1,5 +1,6 @@
 from ScaleDetection import ScaleDetection
 from decimal import *
+import json
 
 
 def testCalibration():
@@ -19,3 +20,25 @@ def testScale():
     w,h = sd.detectSize(image)
     assert round(w, 1) == 30.2
     assert round(h, 1) == 30.2
+
+
+def testSaveConfig():
+    image = 'test.jpg'
+    cf = 'test.conf'
+    sd = ScaleDetection()
+    sd.x_scale = 0.01002450874524511436479390460
+    sd.y_scale = 0.01023352102907033668498759679
+    sd.saveConfigFile(config_file=cf)
+    with open(cf, 'r') as conf:
+        j = json.load(conf)
+    assert j["x_scale"] == sd.x_scale
+    assert j["y_scale"] == sd.y_scale
+
+
+def testLoadConfig():
+    image = 'test.jpg'
+    cf = 'test.conf'
+    sd = ScaleDetection()
+    sd.loadConfigFile(config_file=cf)
+    assert sd.x_scale is not None
+    assert sd.y_scale is not None
