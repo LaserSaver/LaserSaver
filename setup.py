8e249deed07189b22a6dbddb461546c14e96d5c1
luke <lwvyruz@gmail.com>
2015-11-15 13:08:51 -0700

    added setup file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='scale',
+      version='0.1',
+      description='scale detection',
+      package_dir={'scale': 'src'},
+      packages=['scale'])
