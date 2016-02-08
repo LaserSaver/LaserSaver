adb760271ff482d73848ff9c50287883ea757587
Nir Boneh <nboneh5921@gmail.com>
2016-02-07 21:51:06 -0700

    Adding documentation

--- GUI/gui.py
+++ GUI/gui.py
@@ -65,7 +65,7 @@ class App:
 		
 	def updatePanel(self):
 		''' Updates the image in the video capture 
-			panel every 10 milliseconds
+		    panel every 10 milliseconds
 		'''
 		imgtk = ImageTk.PhotoImage(self.getImg())
 		self.panel.configure(image = imgtk)
