4bc770ecc9629d88b409be6be529c66a06d9c39f
jtresman <jacob.resman@colorado.edu>
2016-02-23 17:15:06 -0700

    Stiching Tests

--- test/test_stitch.py
+++ test/test_stitch.py
@@ -1,15 +1,17 @@
 import unittest
+import cv2
+import imutils
 from stitch import Stitcher
 
-imageA = cv2.imread("test/left1.jpg")
-imageB = cv2.imread(args["test/right1.jpg"])
+IMAGEA = cv2.imread("test/left1.jpg")
+IMAGEB = cv2.imread("test/right1.jpg")
 
 def testNoImagesFail():
     pass
 
 def testSimpleImages():
-    imageA = imutils.resize(imageA, width=400, height=400)
-    imageB = imutils.resize(imageB, width=400, height=400)
+    imageA = imutils.resize(IMAGEA, width=400, height=400)
+    imageB = imutils.resize(IMAGEB, width=400, height=400)
 
     # stitch the images together to create a panorama
     stitcher = Stitcher()
