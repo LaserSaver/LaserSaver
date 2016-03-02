c90a6134e341df9dca4aa7151301db80edf9afbb
ZakCollins <foazak@yahoo.com>
2016-03-01 19:00:55 -0700

    move gui into src

--- src/scanner.py
+++ src/scanner.py
@@ -1,10 +1,36 @@
 import findContours
 import scaleDetection
 import stitch
+import gui
+
+scaleDetect = ScaleDetection()
+
+def calibration(image, objx, objy):
+    scaleDetect.calibrate(image, objx, objy)
+
+def gui_start_screen():
+    a = 0
+
+def gui_calibration_screen():
+    a = 0
+
+def gui_take_pictures_screen():
+    a = 0
+
+def gui_enter_thickness_screen():
+    a = 0
+
+#Is this function needed?
+def gui_export_screen():
+    a = 0
 
 def main():
+    gui_start_screen()
+
     #Calibration
-    scaleDetect = ScaleDetection()
+    calibration()
+
+
 
 
 if __name__ == "__main__":
