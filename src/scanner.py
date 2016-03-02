90d58e1b334164558986b368825e7d030c55bfa9
ZakCollins <foazak@yahoo.com>
2016-03-02 16:26:13 -0700

    hook up skew correction calibration

--- src/scanner.py
+++ src/scanner.py
@@ -2,32 +2,38 @@ import findContours
 import scaleDetection
 import stitch
 import gui
+import determineSkew
+import scannerCamera
 
 scaleDetect = ScaleDetection()
-findContours = FindContours()
+#detSkew = DetermineSkew()
+cam1Settings = ScannerCamera() #not implemented yet?
+cam2Settings = ScannerCamera()
 
 def scale_calibration(image, objx, objy):
     success = scaleDetect.calibrate(image, objx, objy)
     scaleDetect.saveConfigFile()
     return success
 
-def calibration_data():
+def scale_calibration_data():
     success = scaleDetect.loadConfigFile()
     #load other calibration data as well
     return success
 
-def skew_calibration(image):
-    a = 0
+def skew_calibration(calibImages):
+    dst, roi, error = DetermineSkew.createSkewMatrix(calibImages)
+    return dst
 
 def skew_correction(image):
     a = 0
 
 def stitch_images(image1, image2):
-    stitcher = Stitcher()
-    return stitcher.stitch((image1,image2)) #correct order for images?
+    #stitcher = Stitcher()
+    return Stitcher.stitch((image1,image2)) #correct order for images?
 
 def find_contours(image):
-    contours, hierarchy, edgeImage = findContours.find_all_contours(image)
+    #findContours = FindContours()
+    contours, hierarchy, edgeImage = FindContours.find_all_contours(image)
     finalContours = findContours.select_contours(contours, hierarchy)
     return finalContours,edgeImage
 
@@ -37,7 +43,10 @@ def export_json(contours):
 def gui_start_screen():
     a = 0
 
-def gui_calibration_screen():
+def gui_scale_calibration_screen():
+    a = 0
+
+def gui_skew_calibration_screen():
     a = 0
 
 def gui_take_pictures_screen():
@@ -55,20 +64,24 @@ def main():
     gui_start_screen()
 
     #load calibration data
-    calData = calibration_data()
+    scaleDataExists = scale_calibration_data()
 
-    #if calibration data doesn't exist, ask user if they want to calibrate.
-    if (not calData):
-        calButton, image, objx, objy = gui_calibration_screen()
-        if (calButton):
-            scale_calibration(image, objx, objy)
-            skew_calibration(image)#probably wrong place for this
-            calData = calibration_data()
+    #if calibration data doesn't exist, ask user to calibrate
+    if (not scaleDataExists):
+        image, objx, objy = gui_scale_calibration_screen()
+        scale_calibration(image, objx, objy)
+
+    #ask user if they want to calibrate if skew calibration data doesn't exist
+    calibImages1, calibImages2 = gui_skew_calibration_screen()
+    dst1 = skew_calibration(calibImages1)
+    dst2 = skew_calibration(calibImages2)
 
     image1, image2 = gui_take_pictures_screen()
 
-    image1 = skew_correction(image1)
-    image2 = skew_correction(image2)
+
+
+    image1 = skew_correction(image1, dst1)
+    image2 = skew_correction(image2, dst2)
 
     finalImage = stitch_images(image1, image2)
     contours, edgeImage = find_contours(finalImage)
