127e068d7c008f753178ca24b6aa9e6c730ffb01
jtresman <jacob.resman@colorado.edu>
2016-03-08 07:57:19 -0700

    OpenCV Install Instructions

--- test/test_stitch.py
+++ test/test_stitch.py
@@ -1,19 +1,51 @@
-import unittest
+from nose.tools import *
 import cv2
 import imutils
 from stitch import Stitcher
 
-IMAGEA = cv2.imread("test/left1.jpg")
-IMAGEB = cv2.imread("test/right1.jpg")
+IMAGEA = cv2.imread("left1.jpg")
+IMAGEB = cv2.imread("right1.jpg")
+IMAGEC = cv2.imread("left2.jpg")
 
-def testNoImagesFail():
-    pass
 
-def testSimpleImages():
+def testStichPass():
     imageA = imutils.resize(IMAGEA, width=400, height=400)
     imageB = imutils.resize(IMAGEB, width=400, height=400)
 
     # stitch the images together to create a panorama
     stitcher = Stitcher()
     (result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
+    # show the images (commented out for using pi through ssh)
+    cv2.imshow("Result", result)
     assert result != None
+
+
+@raises(TypeError)
+def testStichFail():
+    imageA = imutils.resize(IMAGEC, width=400, height=400)
+    imageB = imutils.resize(IMAGEB, width=400, height=400)
+
+    stitcher = Stitcher()
+    (result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
+
+def testDetectAndDescribePass():
+    imageA = imutils.resize(IMAGEA, width=400, height=400)
+    stitcher = Stitcher()
+    (kpsA, featuresA) = stitcher.detectAndDescribe(imageA)
+    assert kpsA != None and featuresA != None
+
+
+def testDetectAndDescirbeFail():
+    pass
+
+def testmatchKeyPointsPass():
+    pass
+
+def testmatchKeyPointsFail():
+    pass
+
+def testdrawMatchesPass():
+    pass
+
+def testdrawMatchesFail():
+    pass
