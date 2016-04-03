58352ba1eef622ac16d319c31bf11059d3a43d38
Nir Boneh <nboneh5921@gmail.com>
2016-04-03 15:38:27 -0600

    Mpre skew work

--- src/GUI/scaleModel.py
+++ src/GUI/scaleModel.py
@@ -1,10 +1,14 @@
 import time
 import sys
+import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import scanner
+from scanner import Scanner
 class ScaleModel:
+	def __init__(self):
+		self.scanner = Scanner()
+
 	def calculate(self, img1, img2, width, height, units):
-		scaleDetectObj = Scanner.scaleCalibration(img1, img2, width, height, units)
+		scaleDetectObj = self.scanner.scaleCalibration(img1, img2, width, height, units)
 
 		#should return scaleDetectObj
 
