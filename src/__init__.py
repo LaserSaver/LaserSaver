b95d9e2697c4beecaf9b9c9fc0813cf88564891b
Alex Cordero <Alex Cordero>
2016-04-10 13:34:36 -0600

    pulled in from integration

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,13 @@
 from scaleDetection import ScaleDetection
+from configcommunicator import configCommunicator
 from contourDetection import contourDetection
 from jsoncreator import jsonCreator
 from logger import logger
 from stitch import Stitcher
+from determineSkew import DetermineSkew
+from scanner import Scanner
+from scannerCamera import ScannerCamera
 
-__all__ = ["logger", "jsonCreator", "ScaleDetection", "ContourDetection", "Stitcher"]
+
+__all__ = ["ScaleDetection", "configCommunicator", "ContourDetection", "jsonCreator", "logger", "Stitcher", \
+            "DetermineSkew", "Scanner", "ScannerCamera"]
