2f0693f38a45fbcf0d6ff379544c55e36a3814af
Nir Boneh <nboneh5921@gmail.com>
2016-04-19 18:14:38 -0600

    Trying sizes for OSes

--- src/GUI/app.py
+++ src/GUI/app.py
@@ -15,15 +15,20 @@ class App:
 #Initializing GUI window
 root = Tk()
 root.wm_title("LaserSaver")
+defaultFont = tkFont.nametofont("TkDefaultFont")
 if platform.system() == 'Windows':
 	root.state('zoomed')
 	root.geometry('{}x{}'.format(640, 480))
+	defaultFont.configure(size=14)
+else if: platform.system() == 'Linux':
+	root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+	defaultFont.configure(size=14)
 else :
-	root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()));
+	root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+	defaultFont.configure(size=18)
 root.minsize(530, 430)
 
 #Setting default font size
 defaultFont = tkFont.nametofont("TkDefaultFont")
-defaultFont.configure(size=18)
 app = App(root)
 root.mainloop()
